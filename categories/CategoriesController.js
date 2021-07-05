705dad1602340a709335b6b93ebe21eaf478b471
iago-s <iagooliveira3845@hotmail.com>
2021-07-05 11:32:11 -0300

    feat: CRUD categorys adm

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,76 @@
 const express = require('express');
 const router = express.Router();
+const slugify = require('slugify');
 
-router.get('/categories', (req, res) => {
-  res.send('Categories router');
+const Category = require('./Category');
+
+router.get('/admin/categories', (req, res) => {
+  Category.findAll().then((categories) => {
+    res.render('admin/categories/index', { categories });
+  });
 });
 
 router.get('/admin/categories/new', (req, res) => {
-  res.send('Categories router new');
+  res.render('admin/categories/new');
+});
+
+router.get('/admin/categories/edit/:id', (req, res) => {
+  const { id } = req.params;
+
+  if (isNaN(id)) {
+    res.redirect('/admin/categories');
+    return;
+  }
+
+  Category.findByPk(id)
+    .then((category) => {
+      if (category != undefined) {
+        res.render('admin/categories/edit', { category });
+      } else {
+        res.redirect('/admin/categories');
+      }
+    })
+    .catch((err) => res.redirect('/admin/categories'));
+});
+
+router.post('/categories/save', (req, res) => {
+  const { title } = req.body;
+
+  if (title != undefined) {
+    Category.create({
+      title,
+      slug: slugify(title),
+    }).then(() => res.redirect('/admin/categories'));
+  } else {
+    res.redirect('/admin/categories/new');
+  }
+});
+
+router.post('/categories/update', (req, res) => {
+  const { id, title } = req.body;
+
+  Category.update(
+    { title, slug: slugify(title) },
+    {
+      where: { id },
+    }
+  ).then(() => res.redirect('/admin/categories'));
+});
+
+router.post('/categories/delete', (req, res) => {
+  const { id } = req.body;
+
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Category.destroy({ where: { id } }).then(() =>
+        res.redirect('/admin/categories')
+      );
+    } else {
+      res.redirect('/admin/categories');
+    }
+  } else {
+    res.redirect('/admin/categories');
+  }
 });
 
 module.exports = router;
